39fd6bf92cd645539d803b638ab93262d680ab09
malkist01 <164641045+malkist01@users.noreply.github.com>
2025-09-26 01:53:15 +0700

    Update build.sh

--- build.sh
+++ build.sh
@@ -4,7 +4,7 @@ rm -rf kernel
 git clone $REPO -b $BRANCH kernel
 cd kernel
 
-curl -LSs "https://raw.githubusercontent.com/malkist01/patch/main/fs/patch.sh" | bash -s main
+curl -LSs "https://raw.githubusercontent.com/malkist01/patch/main/add/patch.sh" | bash -s main
 
 # Add KernelSU
 curl -LSs "https://raw.githubusercontent.com/SukiSU-Ultra/SukiSU-Ultra/main/kernel/setup.sh" | bash -s nongki
